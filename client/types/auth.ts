7aa21166554681a007af57dd5540976bcc101c79
JubrilKolade <jubrilkolade97@gmail.com>
2026-02-28 22:59:41 +0100

    chore: add missing types/components and finalize login page

---
+++ client/types/auth.ts
@@ -0,0 +1,14 @@
+export interface User {
+    id: string;
+    email: string;
+    name: string;
+}
+
+export interface AuthResponse {
+    token: string;
+    user: User;
+}
+
+export interface AuthError {
+    message: string;
+}
